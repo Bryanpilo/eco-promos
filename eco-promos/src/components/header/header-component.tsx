c8933441f14287fe9a0b308b0d63c477a52df8d7
Bryan <bryan220995@gmail.com>
2024-02-14 20:53:02 -0600

    Header with bootstrap

--- eco-promos/src/components/header/header-component.tsx
+++ eco-promos/src/components/header/header-component.tsx
@@ -1,11 +1,38 @@
-import "./header-styles.css"
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import { Link } from "react-router-dom";
 
-const Header = () =>{
-    return(
-        <div className="header-container">
-            <p>header</p>
-        </div>
-    )
-}
+import "./header-styles.css";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Link to={'/'} className='navbar-brand'>React-Bootstrap</Link>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Link to={'/testing'} className='nav-link'>Home</Link>
+              <Nav.Link href="/testing">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
 
 export default Header;
